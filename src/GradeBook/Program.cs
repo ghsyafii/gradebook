903e843c9c8b96f25dd6877df4e7d8fd9ca91bc1
Syafii Ghazali <asy-syafii.ghazali@capgemini.com>
2021-04-27 11:19:40 +0800

    initial commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0) 
+            {
+                Console.WriteLine($"Hello, {args[1]}!");
+            } 
+            else 
+            {
+                Console.WriteLine("No name given.");
+                Console.WriteLine("Please provide name");
+            }
+        }
+    }
+}
+;
